2722f9b670424d75e6b9d85fe766eb1f8b47b9d5
Juriel Villacampa <jurielvillacampa35@gmail.com>
2026-09-25 17:31:01 +0800

    Refactor Vehicle class methods and formatting

--- Vehicle.java
+++ Vehicle.java
@@ -1,23 +1,21 @@
 public class Vehicle {
 
-String brand;
-String model; 
-int year;
+    String brand;
+    String model;
+    int year;
 
-Vehicle(String brand, String model, int year){
-this.brand = brand;
-this.model = model;
-this.year = year;
-}
+    void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println( "Model: " + model);
+         System.out.println("Year: " + year);
+    }
 
-void displayInfo() {
-System.out.println(brand + ", " + model + ", "+ year);
-}
-int calculateAge() {
-return 2026 - year;
-}
-boolean isVintage() {
-return calculateAge() > 25;
-}
+    int calculateAge() {
+        return 2026 - year;
+    }
+
+    boolean isVintage() {
+        return calculateAge() > 25;
+    }
 
     }
