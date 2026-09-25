daae76968683501bacb3c9a52fe55c8682e95c80
Juriel David Villacampa <jurielvillacampa35@gmail.com>
2026-09-25 17:24:17 +0800

    Clean up leftover merge conflict markers

--- Vehicle.java
+++ Vehicle.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Vehicle {
 
 String brand;
@@ -22,26 +21,3 @@ return calculateAge() > 25;
 }
 
     }
-
-=======
-public class Vehicle {
-
-    String brand;
-    String model;
-    int year;
-
-    void displayInfo() {
-        System.out.println("Brand: " + brand);
-        System.out.println( "Model: " + model);
-         System.out.println("Year: " + year);
-    }
-
-    int calculateAge() {
-        return 2026 - year;
-    }
-
-    boolean isVintage() {
-        return calculateAge() > 25;
-    }
-}
->>>>>>> 4f2257e479d697240c7b30a9ec62e5580d01f859
